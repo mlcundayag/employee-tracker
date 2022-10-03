e3e5c8288c2e3bec33d92984e7391e886eefda28
mlcundayag <mlcundayag@gmail.com>
2022-10-03 02:02:41 -1000

    remove classes and completed add employee function

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
-//import classes
-const Department = require('./lib/department');
-const Role = require('./lib/role');
-const Employee = require('./lib/employee');
-
 //import dependencies
 const inquirer = require('inquirer');
 const mySQL = require('mysql2');
-const consoleTable = require('console.table');
+require('console.table');
 const figlet = require('figlet');
 
 //terminal color output style
@@ -64,8 +59,8 @@ const mainMenu = () => {
                 ]
             }
         ])
-        .then((answer) => {
-            const { menu } = answer;
+        .then((data) => {
+            const { menu } = data;
             if (menu === 'View All Employees') {
                 viewEmployees();
             }
@@ -104,10 +99,12 @@ const mainMenu = () => {
             }
             if (menu === 'Exit') {
                 outputSuccessText('Success!\nThanks for using Employee Tracker! 😀 ');
+                db.end()
             }
         })
 }
 
+//View all employees
 const viewEmployees = () => {
     db.query(`SELECT
         employee.id, 
@@ -126,6 +123,143 @@ const viewEmployees = () => {
         mainMenu()
     })
 }
+
+//adding an employee
+const addEmployee = () => {
+    //generate roles list
+    db.query(`SELECT role.id, role.title
+            FROM role;`, (err, roleResults) => {
+        if (err) outputErrorText(err);
+        const roleList = [];
+        roleResults.forEach(({ id, title }) => {
+            roleList.push({
+                name: title,
+                value: id
+            });
+        });
+        //generate managers list
+        db.query(`SELECT * FROM employee;`, (err, manResults) => {
+            if (err) outputErrorText(err);
+            const managerList = [{
+                name: 'None',
+                value: 0
+            }];
+            manResults.forEach(({ first_name, last_name, id }) => {
+                managerList.push({
+                    name: `${first_name} ${last_name}`,
+                    value: id
+                });
+            });
+
+            inquirer
+                .prompt([
+                    {
+                        type: "input",
+                        name: "firstName",
+                        message: `\x1b[33mWhat is the employee's first name?\x1b[0m`,
+                    },
+                    {
+                        type: "input",
+                        name: "lastName",
+                        message: `\x1b[33mWhat is the employee's last name?\x1b[0m`,
+                    },
+                    {
+                        type: 'list',
+                        name: 'role',
+                        message: `\x1b[33mWhat is the employee's role?\x1b[0m`,
+                        choices: roleList
+                    },
+                    {
+                        type: 'list',
+                        name: 'manager',
+                        message: `\x1b[33mWho is the employee's manager?\x1b[0m`,
+                        choices: managerList
+                    }
+                ])
+                .then(data => {
+                    let managerID = data.manager !== 0 ? data.manager : null;
+                    db.query('INSERT INTO employee SET ?', {
+                        first_name: data.firstName,
+                        last_name: data.lastName,
+                        role_id: data.role,
+                        manager_id: managerID
+                    }, (err, res) => {
+                        if (err) throw err;
+                        outputSuccessText(`successfully inserted employee ${data.firstName} ${data.lastName} with id ${res.insertId}`);
+                        viewEmployees()
+                    })   
+                })
+        });
+    })
+}
+
+
+// const addEmployee2 = () => {
+//     return inquirer
+//         .prompt([
+//             {
+//                 type: "input",
+//                 name: "firstName",
+//                 message: `\x1b[33mWhat is the employee's first name?\x1b[0m`,
+//             },
+//             {
+//                 type: "input",
+//                 name: "lastName",
+//                 message: `\x1b[33mWhat is the employee's last name?\x1b[0m`,
+//             }
+//         ])
+//         .then((data) => {
+//             const firstName = data.firstName;
+//             const lastName = data.lastName;
+//             db.query(`SELECT role.id, role.title
+// FROM role;`, (err, results) => {
+//                 if (err) throw err;
+//                 const rolesList = results.map(({ id, title }) => ({ name: title, value: id }));
+//                 inquirer
+//                     .prompt([
+//                         {
+//                             type: 'list',
+//                             name: 'role',
+//                             message: `\x1b[33mWhat is the employee's role?\x1b[0m`,
+//                             choices: rolesList
+//                         }
+//                     ])
+//                     .then(roleChoice => {
+//                         const role = roleChoice.role;
+//                         db.query(`SELECT * FROM employee`, (err, results) => {
+//                             if (err) throw err;
+//                             const managersList = results.map(({ id, first_name, last_name }) => ({ name: `${first_name} ${last_name}`, value: id }));
+//                             inquirer
+//                                 .prompt([
+//                                     {
+//                                         type: 'list',
+//                                         name: 'manager',
+//                                         message: `\x1b[33mWho is the employee's manager?\x1b[0m`,
+//                                         choices: managersList
+//                                     }
+//                                 ])
+//                                 .then(managerChoice => {
+//                                     const manager = managerChoice.manager
+//                                     const newEmployee = new Employee(
+//                                         firstName,
+//                                         lastName,
+//                                         role,
+//                                         manager
+//                                     )
+//                                     outputWelcomeText(newEmployee);
+//                                     employees.push(newEmployee)
+//                                     db.query(`INSERT INTO employee (first_name, last_name, role_id, manager_id)
+//                         VALUES(?)`, [newEmployee], (err, results) => {
+//                                         if (err) throw err;
+//                                         console.table(results)
+//                                     })
+//                                     mainMenu()
+//                                 })
+//                         })
+//                     })
+//             })
+//         })
+// }
 
 
 
