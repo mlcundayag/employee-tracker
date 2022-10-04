cb76679dea0236b0cfdfff5babe2591e8b450935
mlcundayag <mlcundayag@gmail.com>
2022-10-04 01:17:32 -1000

    added bonus feature of changin manager

--- app.js
+++ app.js
@@ -48,18 +48,18 @@ const mainMenu = () => {
                 name: "menu",
                 message: `\x1b[33mWhat would you like to do?\x1b[0m`,
                 choices: [
-                    'View All Employees',
-                    'Add an Employee',
-                    'Update an Employee',
-                    'Remove an Employee',
-                    'View All Roles',
-                    'Add a Role',
-                    'Update a Role',
-                    'Remove a Role',
                     'View All Departments',
+                    'View All Employees',
+                    'View All Roles',
                     'Add a Department',
+                    'Add a Role',
+                    'Add an Employee',
                     'Update a Department',
+                    'Update a Role',
+                    'Update an Employee',
                     'Remove a Department',
+                    'Remove a Role',
+                    'Remove an Employee',
                     'Exit'
                 ]
             }
@@ -222,7 +222,7 @@ const updateEmployee = () => {
             });
         });
         //generate role list
-        db.query(`SELECT * FROM ROLE;`, (err, roleResults) => {
+        db.query(`SELECT * FROM role;`, (err, roleResults) => {
             if (err) outputErrorText(err);
             const roleList = [];
             roleResults.forEach(({ title, id }) => {
@@ -231,35 +231,58 @@ const updateEmployee = () => {
                     value: id
                 });
             });
-            inquirer
-                .prompt([
-                    {
-                        type: "list",
-                        name: "updateEmployee",
-                        message: `\x1b[33mWhich employee do you want to update title?\x1b[0m`,
-                        choices: employeeList
-                    },
-                    {
-                        type: "list",
-                        name: "updateTitle",
-                        message: `\x1b[33mPlease select their new title...\x1b[0m`,
-                        choices: roleList
-                    }
-                ])
-                .then(data => {
-                    db.query(`UPDATE employee SET ? WHERE ?`, [
+            //BONUS: update manager - render manager list
+            db.query(`SELECT * FROM employee;`, (err, managerResults) => {
+                if (err) outputErrorText(err);
+                const managerList = [{
+                    name: 'None',
+                    value: 0
+                }];
+                managerResults.forEach(({ first_name, last_name, id }) => {
+                    managerList.push({
+                        name: `${first_name} ${last_name}`,
+                        value: id
+                    })
+                });
+                inquirer
+                    .prompt([
                         {
-                            role_id: data.updateTitle
+                            type: "list",
+                            name: "updateEmployee",
+                            message: `\x1b[33mWhich employee do you want to update title?\x1b[0m`,
+                            choices: employeeList
                         },
                         {
-                            id: data.updateEmployee
+                            type: "list",
+                            name: "updateTitle",
+                            message: `\x1b[33mPlease select their new title...\x1b[0m`,
+                            choices: roleList
+                        },
+                        //BONUS: update manager - choose from the list
+                        {
+                            type: "list",
+                            name: "updateManager",
+                            message: `\x1b[33mPlease select their new manager...\x1b[0m`,
+                            choices: managerList
                         }
-                    ], (err, res) => {
-                        if (err) throw err;
-                        outputSuccessText(`successfully updated employee ${data.updateEmployee} role with ${data.updataTitle}`);
-                        viewEmployees()
+                    ])
+                    .then(data => {
+                        let managerID = data.updateManager !== 0 ? data.updateManager : null;
+                        db.query(`UPDATE employee SET ? WHERE ?`, [
+                            {
+                                role_id: data.updateTitle,
+                                manager_id: managerID
+                            },
+                            {
+                                id: data.updateEmployee
+                            }
+                        ], (err, res) => {
+                            if (err) throw err;
+                            outputSuccessText(`successfully updated this employee's role and manager`);
+                            viewEmployees()
+                        })
                     })
-                })
+            })
         })
     })
 }
@@ -331,42 +354,42 @@ const addRole = () => {
             });
         });
         inquirer
-        .prompt([
-            {
-                type: "input",
-                name: "title",
-                message: `\x1b[33mWhat is the role's title?\x1b[0m`,
-            },
-            {
-                type: "input",
-                name: "salary",
-                message: `\x1b[33mHow much is the role's salary?\x1b[0m (don't include dollar sign and commas)`,
-                validate: function (name) {
-                    if (isNaN(name) || (!name)) {
-                        outputErrorText("Please enter role's salary (don't include dollar sign and commas)...")
-                    } else {
-                        return true
+            .prompt([
+                {
+                    type: "input",
+                    name: "title",
+                    message: `\x1b[33mWhat is the role's title?\x1b[0m`,
+                },
+                {
+                    type: "input",
+                    name: "salary",
+                    message: `\x1b[33mHow much is the role's salary?\x1b[0m (don't include dollar sign and commas)`,
+                    validate: function (name) {
+                        if (isNaN(name) || (!name)) {
+                            outputErrorText("Please enter role's salary (don't include dollar sign and commas)...")
+                        } else {
+                            return true
+                        }
                     }
+                },
+                {
+                    type: "list",
+                    name: "department",
+                    message: `\x1b[33mWhich department does the role belong to?\x1b[0m`,
+                    choices: deptList
                 }
-            },
-            {
-                type: "list",
-                name: "department",
-                message: `\x1b[33mWhich department does the role belong to?\x1b[0m`,
-                choices: deptList
-            }
-        ])
-        .then(data => {
-            db.query(`INSERT INTO role SET ?`, {
-                title: data.title,
-                salary: data.salary,
-                department_id: data.department
-            }, (err, res) => {
-                if (err) throw err;
-                outputSuccessText(`successfully added ${data.title} with a salary of ${data.salary} and id ${res.insertId}`);
-                viewRoles();
+            ])
+            .then(data => {
+                db.query(`INSERT INTO role SET ?`, {
+                    title: data.title,
+                    salary: data.salary,
+                    department_id: data.department
+                }, (err, res) => {
+                    if (err) throw err;
+                    outputSuccessText(`successfully added ${data.title} with a salary of ${data.salary} and id ${res.insertId}`);
+                    viewRoles();
+                })
             })
-        })
     })
 }
 
@@ -383,40 +406,40 @@ const updateRole = () => {
             })
         });
         inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "updateRole",
-                message: `\x1b[33mWhich title do you want to update salary?\x1b[0m`,
-                choices: roleList
-            },
-            {
-                type: "input",
-                name: "updateSalary",
-                message: `\x1b[33mPlease enter their new salary...\x1b[0m (don't include dollar sign and commas)`,
-                validate: function (name) {
-                    if (isNaN(name) || (!name)) {
-                        outputErrorText("Please enter role's salary (don't include dollar sign and commas)...")
-                    } else {
-                        return true
-                    }
-                }
-            }
-        ])
-        .then(data => {
-            db.query(`UPDATE role SET ? WHERE ?`, [
+            .prompt([
                 {
-                    salary: data.updateSalary
+                    type: "list",
+                    name: "updateRole",
+                    message: `\x1b[33mWhich title do you want to update salary?\x1b[0m`,
+                    choices: roleList
                 },
                 {
-                    id: data.updateRole
+                    type: "input",
+                    name: "updateSalary",
+                    message: `\x1b[33mPlease enter their new salary...\x1b[0m (don't include dollar sign and commas)`,
+                    validate: function (name) {
+                        if (isNaN(name) || (!name)) {
+                            outputErrorText("Please enter role's salary (don't include dollar sign and commas)...")
+                        } else {
+                            return true
+                        }
+                    }
                 }
-            ], (err, res) => {
-                if (err) throw err;
+            ])
+            .then(data => {
+                db.query(`UPDATE role SET ? WHERE ?`, [
+                    {
+                        salary: data.updateSalary
+                    },
+                    {
+                        id: data.updateRole
+                    }
+                ], (err, res) => {
+                    if (err) throw err;
                     outputSuccessText(`successfully updated role's salary with ${data.updateSalary}`);
                     viewRoles()
+                })
             })
-        })
     })
 }
 
@@ -425,7 +448,7 @@ const removeRole = () => {
     db.query(`SELECT * FROM role`, (err, results) => {
         if (err) outputErrorText(err);
         const roleList = [];
-        results.forEach(({ title, id}) => {
+        results.forEach(({ title, id }) => {
             roleList.push({
                 name: title,
                 value: id
@@ -437,23 +460,23 @@ const removeRole = () => {
             });
         }
         inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "delRole",
-                message: `\x1b[33mWhich role do you want to remove?\x1b[0m`,
-                choices: roleList
-            }
-        ])
-        .then(data => {
-            db.query(`DELETE FROM role WHERE id = ?`, 
-            [data.delRole],
-            (err, res) => {
-                outputSuccessText(`successfully remove role ${data.delRole}`);
-                increment();
-                viewRoles();
+            .prompt([
+                {
+                    type: "list",
+                    name: "delRole",
+                    message: `\x1b[33mWhich role do you want to remove?\x1b[0m`,
+                    choices: roleList
+                }
+            ])
+            .then(data => {
+                db.query(`DELETE FROM role WHERE id = ?`,
+                    [data.delRole],
+                    (err, res) => {
+                        outputSuccessText(`successfully remove role ${data.delRole}`);
+                        increment();
+                        viewRoles();
+                    })
             })
-        })
     })
 }
 
@@ -469,25 +492,25 @@ const viewDepartments = () => {
 //add a department
 const addDepartment = () => {
     inquirer
-    .prompt([{
-        type: "input",
-        name: "depName",
-        message: `\x1b[33mWhat is the name of the department?\x1b[0m`,
-    }])
-    .then(data => {
-        db.query(`INSERT INTO department SET ?`, {name: data.depName}, (err, res) => {
-            if (err) throw err;
-            outputSuccessText(`successfully added ${data.depName} with id ${res.insertId}`);
-            viewDepartments();
+        .prompt([{
+            type: "input",
+            name: "depName",
+            message: `\x1b[33mWhat is the name of the department?\x1b[0m`,
+        }])
+        .then(data => {
+            db.query(`INSERT INTO department SET ?`, { name: data.depName }, (err, res) => {
+                if (err) throw err;
+                outputSuccessText(`successfully added ${data.depName} with id ${res.insertId}`);
+                viewDepartments();
+            })
         })
-    })
 }
 
 //update department name
 const updateDepartment = () => {
     //generate department list
     db.query(`SELECT * FROM department;`, (err, depResult) => {
-        if(err) outputErrorText(err);
+        if (err) outputErrorText(err);
         const deptList = [];
         depResult.forEach(({ name, id }) => {
             deptList.push({
@@ -496,33 +519,33 @@ const updateDepartment = () => {
             })
         });
         inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "updateDept",
-                message: `\x1b[33mWhich department do you want to update name?\x1b[0m`,
-                choices: deptList
-            },
-            {
-                type: "input",
-                name: "newNameDept",
-                message: `\x1b[33mPlease enter new name for this department...\x1b[0m`,
-            }
-        ])
-        .then(data => {
-            db.query(`UPDATE department SET ? WHERE ?`, [
+            .prompt([
                 {
-                    name: data.newNameDept
+                    type: "list",
+                    name: "updateDept",
+                    message: `\x1b[33mWhich department do you want to update name?\x1b[0m`,
+                    choices: deptList
                 },
                 {
-                    id: data.updateDept
+                    type: "input",
+                    name: "newNameDept",
+                    message: `\x1b[33mPlease enter new name for this department...\x1b[0m`,
                 }
-            ], (err, res) => {
-                if(err) throw err;
-                outputSuccessText(`successfully change department name to ${data.newNameDept}`);
-                viewDepartments()
+            ])
+            .then(data => {
+                db.query(`UPDATE department SET ? WHERE ?`, [
+                    {
+                        name: data.newNameDept
+                    },
+                    {
+                        id: data.updateDept
+                    }
+                ], (err, res) => {
+                    if (err) throw err;
+                    outputSuccessText(`successfully change department name to ${data.newNameDept}`);
+                    viewDepartments()
+                })
             })
-        })
     })
 }
 
@@ -543,64 +566,22 @@ const removeDepartment = () => {
             });
         }
         inquirer
-        .prompt([
-            {
-                type: "list",
-                name: "delDept",
-                message: `\x1b[33mWhich department do you want to remove?\x1b[0m`,
-                choices: deptList
-            }
-        ])
-        .then(data => {
-            db.query(`DELETE FROM department WHERE id = ?`, [data.delDept], (err, res) => {
-                outputSuccessText(`successfully remove department ${data.delDept}`);
-                increment();
-                viewDepartments();
+            .prompt([
+                {
+                    type: "list",
+                    name: "delDept",
+                    message: `\x1b[33mWhich department do you want to remove?\x1b[0m`,
+                    choices: deptList
+                }
+            ])
+            .then(data => {
+                db.query(`DELETE FROM department WHERE id = ?`, [data.delDept], (err, res) => {
+                    outputSuccessText(`successfully remove department ${data.delDept}`);
+                    increment();
+                    viewDepartments();
+                })
             })
-        })
     })
 }
 
 welcome()
-
-
-
-//What would you like to do?
-// view all employees
-// update employee role
-// view all roles
-// add role
-// view all departments
-// add department
-
-// view all department
-
-
-// all roles
-// id
-// title
-// department
-// salary
-
-// all employees
-// id
-// first name
-// last name
-// title
-// sales
-// salary
-// manager
-
-// add department [input]
-// = added [input] to database
-
-// add role [input]
-// -name
-// -salary
-// -dapartment
-
-// add employee
-// -first name
-// -last name
-// -role
-// -manager
